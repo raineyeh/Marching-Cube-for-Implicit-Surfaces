10208f90ba2966c3bb8b381645a6831c4a6e2dad
Raine Yeh <raine.yeh@gmail.com>
2016-03-05 01:51:12 -0500

    comment out main.cpp dub functions. Those functions should be called in drawer.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -17,12 +17,12 @@ int main(int argc, char **argv)
 {
 	
 	Evaluator evaluator;
-	evaluator.set_equation("x^2+y"); //this should be called by the drawer as user input
+	//evaluator.set_equation("x^2+y"); //this should be called by the drawer as user input
 
 	Marching march_maker;
 	march_maker.set_evaluator(&evaluator);
 	//march_maker.set_grid_step_size(.02);
-	march_maker.recalculate();	
+	//march_maker.recalculate();	
 
 	Drawer drawer(&argc, argv);
 	drawer.set_march(&march_maker);
