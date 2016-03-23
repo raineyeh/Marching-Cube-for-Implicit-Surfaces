89097e4242c5125a8a93f4c1bb2577e0018237d6
dana Rushaidat <dana.rushaidat@gmail.com>
2016-03-23 01:12:26 -0400

    seeding changes

--- Source/marching.h
+++ Source/marching.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include "evaluator.h"
+#include <queue>
+#include <set>
 
 struct Step_Data{
 	int step_i; //indicates which step this is at. 0~n*n: step count down. -1: finished, -2:not started
@@ -11,7 +13,46 @@ struct Step_Data{
 	std::vector<float> corner_values; //values of the 8 corners. size=8
 	std::vector<float> intersect_coord; //intersection coordinate for each edge. size = 3* number of intersect points
 	std::vector<int> tri_vlist; //vertex list of the triangles. size = 3*num_triangles. max_num_tri = 5
+	std::vector<int> edge_list;
 	float surf_constant; //value of the surface. marching cube corner is considered positive if corner_values[i] > surf_constant
+
+};
+struct vertex_list
+{
+	float x0, x1, y0,  y1, z0, z1;
+	bool operator<(const vertex_list&rhs) const {
+		if (x0 == rhs.x0) 
+		{
+			if (y0 == rhs.y0)
+			{
+				return z0 < rhs.z0;
+			}
+			else
+				return y0 < rhs.y0;
+		}
+		else
+			return x0 < rhs.x0;
+	   }
+	bool operator>(const vertex_list&rhs) const {
+		if (x0 == rhs.x0)
+		{
+			if (y0 == rhs.y0)
+			{
+				return z0 > rhs.z0;
+			}
+			else
+				return y0 > rhs.y0;
+		}
+		else
+			return x0 > rhs.x0;
+	}
+	bool operator==(const vertex_list&rhs) const {
+		if ((x0 == rhs.x0) && (y0 == rhs.y0 )&& (z0 == rhs.z0))
+			return true;
+		else
+			return false;
+		
+	}
 };
 
 struct Poly_Data{
@@ -35,16 +76,18 @@ public:
 	void set_surface_constant(float); //Defines at what value the surface is drawn. Default to 0
 	bool set_surface_repeat_step_distance(float); //must be positive, and best not too low, depending on grid size.
 	bool repeating_surface_mode(bool);
-
+    void seed_mode(bool);//this to set the seed mode to true or false 
+	bool set_seed(float x, float y, float z);   //reading the seeed from the UI and checking 
+	void get_seed_grid();  //to find where is the starting grid for the seed point 
 private:
-	
+			
 	float evaluate(float x, float y, float z);
 	void do_square(float, float, float, float, float, float);
 	int add_triangle(int, int, int);
 	int add_point(float, float, float);
 	void add_step_to_poly_data();
 	float interp(float, float, float, float);
-
+	void find_cubes_for_seeding();
 	void print_step_info();
 
 	Evaluator* evaluator;
@@ -54,7 +97,12 @@ private:
 	float surface_constant;
 	float surface_step;
 	bool is_repeating_surface;
-
+	bool is_seed_mode;
+	float seed[3] ;
+	vertex_list seed_grid;
+	bool cubes[26];
+	queue <vertex_list> seed_queue; 
+	set<vertex_list> my_seed_set;
 };
 
 #endif
